cef0b7351fd7af8495b9e07ea5c3b4eb96e4b1a8
Yakira Baruch <yakirabaruch@Yakiras-MacBook-Pro.local>
2017-06-01 17:03:34 -0400

    Update

--- Assets/js/trivia.js
+++ Assets/js/trivia.js
@@ -10,7 +10,7 @@ function populate() {
 		//show question	
 
 		var element= documents.getElementbyId("question");
-		element.innerHTML=quiz.getQuestionIndex().text;
+		element.innerHtml=quiz.getQuestionIndex().text;
 
 		//show choices
 			var choices = quiz.getQuestionIndex().choices;
@@ -23,11 +23,12 @@ function populate() {
 
 function guess(id, guess) {
 	var button = document.getElementbyId();
-	button.onclick = function(){
+	$("button").on("click",function()){
 		quiz.guess(guess);
 		populate();
 	}
-}
+};
+
 
 function showScores(){
 		var gameOverHtml= "<h1>Results</h1>";
@@ -35,15 +36,20 @@ function showScores(){
 		var element = document.getElementbyId("quiz");
 		element.innerHTML= gameOverHtml;
 		guess ("btn" + i, choices[i]);
-}
+	}
+
+		showProgress();
+
+	}
+};
 
 
 	var questions = [
-	new Question ("What does Abu try to steal from the Cave of Wonders?",["A hat","A carpet","A ruby","A crown"] "A ruby"),
-	new Question ("What does Ariel collect?", ["Eels", "Shells", "Coins", "Human Items"] "Human Items"),
-	new Question ("What is the name of the mouse Cinderella rescues?",["Gus", "Vinny", "Tootie", "Chad"] "Gus"),
-	new Question ("What is name of Simba's mother?", ["Nala", "Zazu", "Sarabi", "Sarafina"] "Sarabi"),
-	new Question ("Who is Buzz Lightyear's arch-memesis?"["Supreme Leader Snort", "Emperor Zorg", "King Darkness", "Voldermort",] "Emperor Zorg"),
+	new Question ("What does Abu try to steal from the Cave of Wonders?",["A hat","A carpet","A ruby","A crown"] "A ruby");
+	new Question ("What does Ariel collect?", ["Eels", "Shells", "Coins", "Human Items"] "Human Items");
+	new Question ("What is the name of the mouse Cinderella rescues?",["Gus", "Vinny", "Tootie", "Chad"] "Gus");
+	new Question ("What is name of Simba's mother?", ["Nala", "Zazu", "Sarabi", "Sarafina"] "Sarabi");
+	new Question ("Who is Buzz Lightyear's arch-memesis?"["Supreme Leader Snort", "Emperor Zorg", "King Darkness", "Voldermort",] "Emperor Zorg");
 
 	];	
 
